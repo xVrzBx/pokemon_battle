2e7ddbb59d7c51546302776cb06258257445cdbc
xVrzBx <bryancs4899@gmial.com>
2022-09-03 12:45:33 -0500

    Create health bar and fix damage

--- batallapkm.c
+++ batallapkm.c
@@ -25,15 +25,29 @@ int main(){
     int const blue_berry = 20;
     int const water_gun = 10;
     int atack=0;
+    //Hbar
+    int bar1, bar2;
     printf("Battle starts\n");
     //Battle starts
     while(1){
         srand(time(0));
+        //Squirtles Heald bar
+        bar2= ph2*10/PH2;
+        printf("Squirtle PH:%d[", ph2);
+        for(int x=0; x<bar2; x++)printf("#");
+        for(int x=0; x<10-bar2; x++)printf(" ");
+        printf("]\n");
+        //Charmander heald bar
+        bar1= ph1*10/PH1;
+        printf("Charmander PH:%d[", ph1);
+        for(int x=0; x<bar1; x++)printf("#");
+        for(int x=0; x<10-bar1; x++)printf(" ");
+        printf("]\n");
+
         printf("Squirtles turn\n");
         atack = rand()%4+1;
         printf("%d\n", atack);
         printf("Press enter to continue...\n");
-        getchar();
         //squirtles turn
         if(atack==1){
             printf("Squirtles uses burble\n");
@@ -54,27 +68,44 @@ int main(){
             printf("charmander recibes damage\n");
         }
         if(ph1<0)ph1=0;
+        getchar();
+        system("clear");
         if(ph1==0)break;
-        
+
         printf("Your turn\n");
         do{
+             //Squirtles Heald bar
+            bar2= ph2*10/PH2;
+            printf("Squirtle PH:%d[", ph2);
+            for(int x=0; x<bar2; x++)printf("#");
+            for(int x=0; x<10-bar2; x++)printf(" ");
+            printf("]\n");
+            //Charmander heald bar
+            bar1= ph1*10/PH1;
+            printf("Charmander PH:%d[", ph1);
+            for(int x=0; x<bar1; x++)printf("#");
+            for(int x=0; x<10-bar1; x++)printf(" ");
+            printf("]\n");
+            
             printf("Chose an option\n");
             printf("1)ember\n2)scratch\n3)metal claw\n4)sleep\n");
             fflush(stdin);
             scanf("%d", &atack);
+            system("clear");
+
         }while(atack!=1 && atack != 2 && atack != 3 && atack != 4);
         //squirtles turn
         if(atack==1){
             printf("Charmander uses ember\n");
-            ph1-=ember;
+            ph2-=ember;
             printf("Squiertle recibes damage\n");
         }else if(atack==2){
             printf("Charmander uses scratch\n");
-            ph1-=scratch;
+            ph2-=scratch;
             printf("Squiertle recibes damage\n");
         }else if(atack==3){
             printf("Charmander uses metal claw\n");
-            ph1-=metal_claw;
+            ph2-=metal_claw;
             printf("Squiertle recibes damage\n");
         }else{
             printf("Charmander uses sleep\n");
@@ -82,9 +113,14 @@ int main(){
         }
         if(ph1<0)ph1=0;
         getchar();
+        system("clear");
 
         if(ph2==0)break;
-
+    }
+    if(ph1>ph2){
+        printf("You win!\nCharmander recibes 4xp\n");
+    }else{
+        printf("Squirtles win!\nCharmander recibes 1xp\n");
     }
     return 0;
 }
